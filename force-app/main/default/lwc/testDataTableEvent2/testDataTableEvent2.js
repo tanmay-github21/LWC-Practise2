e205d39606ec6cb5e4503fa928bc38c2d44d72a4
Tanmay Dash <ilove2boogycode@Gmail.com>
2022-07-15 21:35:49 +0530

    Added api decorator for capturing the id in console

--- force-app/main/default/lwc/testDataTableEvent2/testDataTableEvent2.js
+++ force-app/main/default/lwc/testDataTableEvent2/testDataTableEvent2.js
@@ -6,6 +6,7 @@ export default class TestDataTableEvent2 extends LightningElement
     accounts1; 
     columns=columns; 
     @track c1;
+    @api copyValue;
     @track c2;
     @api showAccount()
     {
@@ -16,7 +17,10 @@ export default class TestDataTableEvent2 extends LightningElement
 
     handleRowSelection=event=>
     {
-        this.c1= event.detail.selectedRows; 
-        console.log('Console log data table',this.c1); 
+        this.copyValue= event.detail.selectedRows; 
+        console.log('Console log Hello From Child Component 4654',this.copyValue); 
+        const childConst= new customEvent('datatable', {detail:this.c1});
+        this.dispatchEvent(childConst); 
+        console.log('Console log event firederty gkrgnr 454554'); 
     }
 }
