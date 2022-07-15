8a540511e127281b5dc5098908ad09594418c88c
Tanmay Dash <ilove2boogycode@Gmail.com>
2022-07-15 19:11:27 +0530

    Basic Example Of Track Property

--- force-app/main/default/lwc/testDataTableEvent/testDataTableEvent.js
+++ force-app/main/default/lwc/testDataTableEvent/testDataTableEvent.js
@@ -1,22 +1,13 @@
 import { LightningElement, api, track, wire} from 'lwc';
 import active_Accounts from '@salesforce/apex/active_Inactive_Accounts.active_Accounts';
 const columns=[{label:"Id", fieldName:"Id"}, {label:"Name", fieldName:"Name"}];
-export default class TestDataTableEvent extends LightningElement {
-    columns=columns; 
-    @track user_Input; 
-    accounts1;
-    @wire(active_Accounts)
-    wiredMethod({data, error})
+export default class TestDataTableEvent extends LightningElement 
+{
+    @track parameter; 
+    showMe()
     {
-         if(data)
-         this.accounts1=data;
-         else if (error)
-         this.error=error; 
-    }
-    copyValue = event=>
-    {
-        console.log('Console Log Entered Into Data Table Method');
-        var capturedId= event.detail.selectedRows; 
-        console.log('Console Log JSON Convert'+'\t'+Json.stringify(capturedId)); //this showed an NoErrorObjectAvailable error.
+        this.parameter='Hello Peter'; 
+        console.log('console log entered into method'+'\t'+this.parameter);
     }
 }
+    
