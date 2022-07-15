0c27f32e0d542d932ff777a25232c7bc6471ec47
Tanmay Dash <ilove2boogycode@Gmail.com>
2022-07-15 16:37:54 +0530

    Tried With Local Event

--- force-app/main/default/lwc/dataTableChild/dataTableChild.js
+++ force-app/main/default/lwc/dataTableChild/dataTableChild.js
@@ -3,33 +3,42 @@ import active_Accounts from '@salesforce/apex/active_Inactive_Accounts.active_Ac
 import inactive_Accounts from '@salesforce/apex/active_Inactive_Accounts.inactive_Accounts';
 const columns= [{label:'Account Name', fieldName:'Name'}, {label:'Id', fieldName:'Id'}, {label:'Status', fieldName:'Active__c'}];
 export default class DataTableChild extends LightningElement {
-     columns=columns; 
-     accounts1; 
-     accounts2;
-     inactive_AccountId='';
-     @api selectedAccountId; 
-     @track error; 
+    columns=columns; 
+    @track user_Input;
+    accounts1; 
+    accounts2;
+    selectedAccountId=''; 
+    @api message;
+    @track error; 
 
-     @api activeAccount()
-     {
+    @api activeAccount()
+    {
          console.log('Active Account Child Method')
          active_Accounts()
          .then(result=> {this.accounts1=result; console.log('Result',result);})
          .catch(error=> {this.error=error; console.log('Error Occured'+'\t'+error);}); 
-     }
+    }
 
-     @api inactiveAccount()
-     {
+    @api inactiveAccount()
+    {
         console.log('Inactive Account child method')
          inactive_Accounts()
          .then(result=> {this.accounts2=result; console.log('Result',result);})
          .catch(error=> {this.error=error; console.log('Error Occured'+'\t'+error);}); 
-     }
+    }
 
-     handleRowSelection= event=> 
-     {
+    handleRowSelection= event=> 
+    {
          this.selectedAccountId= event.detail.selectedRows; 
-         const passParent= new customEvent('parentpush',{detail:this.selectedAccountId});
-         this.dispatchEvent(passparent);
-     }
+         console.log('Selected id event',this.selectedAccountId+'\n'+'Event Details'+'\t'+event.detail.selectedRows);
+         const passParent= new customEvent('parentpush',{detail:{message:this.selectedAccountId}});
+         this.dispatchEvent(passParent);
+    }
+
+    nameChange= event=> 
+    {
+        this.user_Input= event.target.value; 
+        console.log('Text Input'+'\t'+this.user_Input); 
+
+    }
 }
