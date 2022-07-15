86ef0838f5859591ac8cbe04e8ca36b54c5152c1
Tanmay Dash <ilove2boogycode@Gmail.com>
2022-07-15 20:20:52 +0530

    Used the track decorator to copy the value of input field

--- force-app/main/default/lwc/testDataTableEvent/testDataTableEvent.js
+++ force-app/main/default/lwc/testDataTableEvent/testDataTableEvent.js
@@ -3,11 +3,21 @@ import active_Accounts from '@salesforce/apex/active_Inactive_Accounts.active_Ac
 const columns=[{label:"Id", fieldName:"Id"}, {label:"Name", fieldName:"Name"}];
 export default class TestDataTableEvent extends LightningElement 
 {
-    @track parameter; 
-    showMe()
+    // @track parameter1; 
+    // @track parameter2; 
+    // showMe= event=> 
+    // {
+    //     // this.parameter1=event.target.value; 
+    //     this.parameter1=event.target.value;
+    //     console.log('Console log'+'\t'+event.target.value); 
+    // }
+    @track changedName; 
+    @track user_Input;
+    capitalized_Input
+    nameChange(event)
     {
-        this.parameter='Hello Peter'; 
-        console.log('console log entered into method'+'\t'+this.parameter);
+        this.user_Input=event.target.value;
+        // this.capitalized_Input=string.toUpperCase(this.user_Input);
     }
 }
     
