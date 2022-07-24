06609c6189317f724910e933c7e34e4002675aee
Tanmay Dash <ilove2boogycode@Gmail.com>
2022-07-24 15:03:31 +0530

    Worked Atlast. Parent received the accountid and shows the related cases

--- force-app/main/default/lwc/imperativeChild/imperativeChild.js
+++ force-app/main/default/lwc/imperativeChild/imperativeChild.js
@@ -6,8 +6,10 @@ const actions=[{label:'View Cases', name:'viewCases'},
                {label:'View Record', name:'viewRecord'}];
 
 const columns=[{label:'Name', fieldName:'Name'}, 
+               {label:'Date Created', fieldName:'CreatedDate'},
                {label:'Status', fieldName:'Active__c'}, 
                {label:'Id', fieldName:'Id'}, 
+               {label:'Type', fieldName:'Type'},
                {type:'action', typeAttributes:{rowActions:actions}}];
 export default class ImperativeChild extends LightningElement {
      
@@ -18,20 +20,20 @@ export default class ImperativeChild extends LightningElement {
      text;
      columns=columns; 
      @api publicProperty;
-     @api caseArray;
+     @api publicProperty2; 
+     @api caseArray=[];
+     @api caseArray2='';
      @track error; 
      @api handleValueChange()
      {
-         console.log('Called By Parent');
+         console.log('Active Account Method Executed By Child');
          active_Accounts()
-         .then(result=> {this.accounts1=result; console.log('Result',result);})
+         .then(result=> {this.accounts1=result; console.log(this.accounts1);})
          .catch(error=> {this.error=error; console.log('Error Occured'+'\t'+error);});
-         //cnjcfnj 
      }
 
      @api handleValueChange2()
      {
-         console.log('Called By Parent Inactive Account'); 
          inactive_Accounts()
          .then(result=> {this.accounts2=result;})
          .catch(error=>{this.error=error; console.log('Error Occured'+'\t'+error);}); 
@@ -53,10 +55,43 @@ export default class ImperativeChild extends LightningElement {
         this.publicProperty=cleanedId.replace(/^,/,''); 
         console.log(this.publicProperty);
         caselist({accountId:this.publicProperty})
-        .then(result=> {this.caseArray=result; console.log(this.caseArray);})
-        .catch(error=>{this.error=error; console.error(error);});
-        const passParent3= new CustomEvent('casearray',{detail:this.caseArray}); 
-        this.dispatchEvent(passParent3);
-        
+        .then (result=> 
+        {
+            this.cases= result; 
+            console.log('Console log within result method',result);
+            result.forEach(element => {
+                const cars={};
+                cars.CaseNumber=element.CaseNumber;
+                cars.Id= element.Id;
+                const set_Array= new Set([cars.Id, cars.CaseNumber]);
+                // const arr=Array.from(set_Array);
+                this.caseArray= Array.from(set_Array);
+             });
+             const passParent3= new CustomEvent('casearray',{detail:this.caseArray}); 
+             console.log('Child Component Before Firing the event'+'\t'+this.caseArray.length); 
+             this.dispatchEvent(passParent3); 
+
+
+        })
+        .catch(error=>
+        {
+            this.error= error;
+            console.error('Error Occured',error);
+        })
     }
+    event_CaseListner2= event=> 
+    {
+        console.log('Inactive method');
+        var selectedId2= this.template.querySelector("lightning-datatable").getSelectedRows(); 
+        let cleanedId2= '';
+        selectedId2.forEach(element => {cleanedId2=cleanedId2+','+element.Id});
+        console.log(cleanedId2); 
+        this.publicProperty2=cleanedId2.replace(/^,/,''); 
+        console.log(this.publicProperty2);
+        // caselist({accountId:this.publicProperty2})
+        // .then (result2=>{this.caseArray22= result2; console.log('Console log within result method',result2);})
+        // .catch(error=>{this.error=error; console.error('Console error occured while fetching cases'+'\t'+error);}); 
+        const passParent4= new CustomEvent('casesforinactives',{detail:this.publicProperty2});
+        this.dispatchEvent(passParent4);
+    } //end of event_CaseListner2
 }
--- force-app/main/default/lwc/imperativeParent/imperativeParent.js
+++ force-app/main/default/lwc/imperativeParent/imperativeParent.js
@@ -1,5 +1,7 @@
 import { LightningElement, api, track} from 'lwc';
+import caselist from '@salesforce/apex/active_Inactive_Accounts.caselist';
 const columns=[{label:'Account Name', fieldName:'Name'}];
+const casecolumns=[{label:"CaseNumber", fieldName:"CaseNumber"}, {label:"Case-Id", fieldName:"Id"}, {label:"Status", fieldName:"Status"}];
 export default class ImperativeParent extends LightningElement {
     @track accounts1;
     @track selectedRows; 
@@ -7,7 +9,10 @@ export default class ImperativeParent extends LightningElement {
     @track message; 
     @track message2; 
     @track message3;
+    @track message4;
+    @track message5
     columns=columns; 
+    casecolumns= casecolumns;
     // constructor()
     // {
     //     console.log('Console log parent constructor called'); 
@@ -32,8 +37,19 @@ export default class ImperativeParent extends LightningElement {
     }
     handlePassCase=event=> 
     {
-        console.log('Console log parent casemethod'); 
+        console.log('Console log entered into parent casemethod handling'); 
         const const3= event.detail;
         this.message3= const3; 
+        console.log('Console Log parent Received the following'+'\t'+this.message3);
+    }
+    handleAccounts_TypeNull=event=>
+    {
+       console.log('Entering into Parent event handling for inactive accounts'); 
+       const const4= event.detail;
+       this.message4= const4; 
+       caselist({accountid:this.message4})
+       .then(result2=>{this.message5=result2; console.log('Parent Caselist',this.message5);}) 
+       .catch(error=>{this.error=error;})
+       console.log('Const-4-Parent',const4+'\n'+this.message4); 
     }
 }
